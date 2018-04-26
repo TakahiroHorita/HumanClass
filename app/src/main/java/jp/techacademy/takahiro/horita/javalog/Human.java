8f4a93ba35552a14422e6a0d58eec60bc308cc9c
TakahiroHorita <37589728+TakahiroHorita@users.noreply.github.com>
2018-04-26 10:28:13 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takahiro/horita/javalog/Human.java
@@ -0,0 +1,22 @@
+package jp.techacademy.takahiro.horita.javalog;
+
+import android.util.Log;
+
+public class Human extends Animal implements Thinkable{
+
+    public Human(String name, int age, String hobby) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    @Override
+    public void say() {
+        Log.d("javatest","「私の名前は" + name + "です。年は" + age + "歳です。」");
+    }
+
+    @Override
+    public void think() {
+        Log.d("javatest", "「私は" + hobby + "について考える」");
+    }
+}
---
+++ app/src/main/java/jp/techacademy/takahiro/horita/javalog/Thinkable.java
@@ -0,0 +1,5 @@
+package jp.techacademy.takahiro.horita.javalog;
+
+public interface Thinkable {
+    public void think();
+}
