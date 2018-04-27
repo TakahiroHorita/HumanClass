1bdcdd6f07d40bfdaaa82aa8d4117f27d0c0113b
TakahiroHorita <takahiro.horita01@gmail.com>
2018-04-27 21:40:02 +0900

    Animalクラスに記述していたメンバ変数hobbyを削除して、Humanクラスに記述。

--- app/src/main/java/jp/techacademy/takahiro/horita/javalog/Human.java
+++ app/src/main/java/jp/techacademy/takahiro/horita/javalog/Human.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 public class Human extends Animal implements Thinkable{
 
+    String hobby; //趣味
+
     public Human(String name, int age, String hobby) {
         this.name = name;
         this.age = age;
